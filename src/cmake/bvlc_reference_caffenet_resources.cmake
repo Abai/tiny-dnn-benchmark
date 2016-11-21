2c69729eda55e50a5ac0d796813fc2b4bab23d27
Aziz Baibabaev <aziz.bbb@gmail.com>
2016-11-21 11:37:02 +0100

    Switch to caffe image preprocessing
    - Use ImageData layer with CMake config to preprocess cat.jpg
    - Adjust prototxt to prevent reuse top/bottom blobs between layers for verification

--- src/cmake/bvlc_reference_caffenet_resources.cmake
+++ src/cmake/bvlc_reference_caffenet_resources.cmake
@@ -1,7 +1,10 @@
 # Define benchmark resources
 set(MODEL_MEAN_PATH "${CMAKE_SOURCE_DIR}/caffe/data/ilsvrc12/imagenet_mean.binaryproto")
 set(MODEL_PATH "${CMAKE_SOURCE_DIR}/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel")
-set(PROTO_PATH "${CMAKE_SOURCE_DIR}/caffe/models/bvlc_reference_caffenet/deploy.prototxt")
+set(PROTO_PATH_IN "${CMAKE_SOURCE_DIR}/benchmarks/bvlc_reference_caffenet.prototxt.in")
+set(PROTO_PATH "${CMAKE_CURRENT_BINARY_DIR}/bvlc_reference_caffenet.prototxt")
+set(SOURCE_TXT_PATH_IN "${CMAKE_SOURCE_DIR}/benchmarks/bvlc_reference_caffenet_sources.txt.in")
+set(SOURCE_TXT_PATH "${CMAKE_CURRENT_BINARY_DIR}/bvlc_reference_caffenet_sources.txt")
 set(TEST_IMAGE_PATH "${CMAKE_SOURCE_DIR}/caffe/examples/images/cat.jpg")
 
 # Download imagenet_mean if required 
@@ -11,8 +14,6 @@ if(NOT EXISTS ${MODEL_MEAN_PATH})
   if(NOT EXISTS ${MODEL_MEAN_PATH})
     message(FATAL_ERROR "Failed to download imagenet_mean.binaryproto to ${MODEL_MEAN_PATH}")
   endif()
-else()
-  add_definitions(-DMODEL_MEAN_PATH="${MODEL_MEAN_PATH}")
 endif()
 
 # Download bvlc_reference_caffenet model if required
@@ -26,15 +27,24 @@ else()
   add_definitions(-DMODEL_PATH="${MODEL_PATH}")
 endif()
 
-# Verify test image and model proto can be found 
+if(NOT EXISTS ${TEST_IMAGE_PATH})
+  message(FATAL_ERROR "Could not find test image at ${TEST_IMAGE_PATH}")
+endif()
+
+# Configure prototxt ImageData layer and test image source txt
+configure_file(${PROTO_PATH_IN} ${PROTO_PATH})
+configure_file(${SOURCE_TXT_PATH_IN} ${SOURCE_TXT_PATH})
+
+# Verify model prototxt can be found 
 if(NOT EXISTS ${PROTO_PATH})
   message(FATAL_ERROR "Could not find prototxt at ${PROTO_PATH}")
 else()
   add_definitions(-DPROTO_PATH="${PROTO_PATH}")
 endif()
 
-if(NOT EXISTS ${TEST_IMAGE_PATH})
-  message(FATAL_ERROR "Could not find test image at ${TEST_IMAGE_PATH}")
-else()
-  add_definitions(-DTEST_IMAGE_PATH="${TEST_IMAGE_PATH}")
+# Verify sources txt can be found 
+if(NOT EXISTS ${SOURCE_TXT_PATH})
+  message(FATAL_ERROR "Could not find sources txt at ${SOURCE_TXT_PATH}")
 endif()
+
+
