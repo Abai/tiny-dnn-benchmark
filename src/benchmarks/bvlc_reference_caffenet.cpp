9bc6dc16974f50cceb74b0e26fc57b1d3dd24c19
Aziz Baibabaev <aziz.bbb@gmail.com>
2016-11-21 13:40:33 +0100

    Add caffe layers benchmark

--- src/benchmarks/bvlc_reference_caffenet.cpp
+++ src/benchmarks/bvlc_reference_caffenet.cpp
@@ -8,10 +8,6 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include "caffe/caffe.hpp"
-//#include "caffe/util/io.hpp"
-//using namespace std; // required for upgrade_proto.hpp
-//#include "caffe/util/upgrade_proto.hpp"
-//#include "caffe/proto/caffe.pb.h"
 
 #include "tiny_dnn/tiny_dnn.h"
 
@@ -19,6 +15,8 @@ using namespace caffe;
 using namespace tiny_dnn;
 using namespace tiny_dnn::activation;
 using namespace tiny_dnn::layers;
+
+using BlobVec = std::vector<Blob<float>*>;
 
 namespace {
 
@@ -33,8 +31,6 @@ namespace {
 
 class BVLCReferenceCaffenet : public ::benchmark::Fixture {
  public:
-  // You can remove any or all of the following functions if its body
-  // is empty.
 
   BVLCReferenceCaffenet() {
     // Initialization happens once per run
@@ -43,12 +39,7 @@ class BVLCReferenceCaffenet : public ::benchmark::Fixture {
     }
   }
 
-  virtual ~BVLCReferenceCaffenet() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
+  virtual ~BVLCReferenceCaffenet() {}
 
   static void SetUpTestCase() {
     // Read Caffe's model prototxt file and upgrade if needed
@@ -60,54 +51,49 @@ class BVLCReferenceCaffenet : public ::benchmark::Fixture {
     ReadNetParamsFromBinaryFileOrDie(model_path, sWeights.get());
 
     // Load caffe net
-    Net<float> caffe_net(proto_path, TEST);
-    caffe_net.CopyTrainedLayersFrom(model_path);
+    sCaffeNet.reset(new Net<float>(proto_path, TEST));
+    sCaffeNet->CopyTrainedLayersFrom(model_path);
 
-    // Perform one forward pass to fill blobs
-    const std::vector<Blob<float>*>& result = caffe_net.Forward();
-
-//    // Fill pointers to layers
-//    sConv1 = 
+    //// Perform one forward pass to fill blobs
+    sCaffeNet->Forward();
 
     sIsInit = true;
   }
 
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
-
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
-
-  // Objects declared here can be used by all tests in the test case for Foo.
   static bool sIsInit;
   static std::unique_ptr<NetParameter> sProto;
   static std::unique_ptr<NetParameter> sWeights;
-  static std::vector<float> sTestFeatures;
+
+  static std::unique_ptr<Net<float>> sCaffeNet;
 };
 
 bool BVLCReferenceCaffenet::sIsInit = false;
 std::unique_ptr<NetParameter> BVLCReferenceCaffenet::sProto; 
 std::unique_ptr<NetParameter> BVLCReferenceCaffenet::sWeights; 
-std::vector<float> BVLCReferenceCaffenet::sTestFeatures;
 
-BENCHMARK_F(BVLCReferenceCaffenet, FooTest)(benchmark::State& st) {
-  while (st.KeepRunning()) {
-    std::string mystring;
+std::unique_ptr<Net<float>> BVLCReferenceCaffenet::sCaffeNet;
+
+static void CaffeLayers(benchmark::internal::Benchmark* b) {
+  int num_layers = BVLCReferenceCaffenet::sCaffeNet->layers().size();
+  for(int i = 1; i < num_layers; ++i) {
+    b->Args({i});
   }
 }
 
-BENCHMARK_DEFINE_F(BVLCReferenceCaffenet, BarTest)(benchmark::State& st) {
-  while (st.KeepRunning()) {
-    std::string mystring = "bla";
+BENCHMARK_DEFINE_F(BVLCReferenceCaffenet, CaffeLayerTest)(
+  benchmark::State& state) {
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+    unsigned int i = state.range(0);
+    auto layer = sCaffeNet->layers().at(i);
+    auto bottom_vec = sCaffeNet->bottom_vecs().at(i);
+    auto top_vec = sCaffeNet->top_vecs().at(i);
+    state.ResumeTiming();
+    layer->Forward(bottom_vec, top_vec);
   }
 }
-/* BarTest is NOT registered */
-BENCHMARK_REGISTER_F(BVLCReferenceCaffenet, BarTest)->Threads(2);
-/* BarTest is now registered */
+
+BENCHMARK_REGISTER_F(BVLCReferenceCaffenet, CaffeLayerTest)->Apply(CaffeLayers);
 
 }  // namespace
 
